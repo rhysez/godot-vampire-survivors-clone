6c04364025f414d3ebd64deae8e5ccf5a8ee15bb
Rhys <121772484+rhysez@users.noreply.github.com>
2025-09-20 11:17:42 +0100

    Initial commit

---
+++ player.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+func _physics_process(delta: float) -> void:
+	var direction = Input.get_vector(
+		"move_left", 
+		"move_right", 
+		"move_up", 
+		"move_down"
+	)
+	velocity = direction * 600
+	move_and_slide()
+	
+	var happy_boo = %HappyBoo
+	if (velocity.length() > 0):
+		happy_boo.play_walk_animation()
+	else:
+		happy_boo.play_idle_animation()
+		
+	
